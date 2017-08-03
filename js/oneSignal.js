a8875fc483415d6d040141784ed5c05f6d223092
bbrassart <baptiste.brassart@gmail.com>
2017-08-03 15:10:08 +0200

    First AppSignal setup

---
+++ js/oneSignal.js
@@ -0,0 +1,8 @@
+var OneSignal = window.OneSignal || [];
+OneSignal.push(["init", {
+  appId: "c81c260c-d7c6-41f6-8333-4f8bb58d07ba",
+  autoRegister: false,
+  notifyButton: {
+    enable: true
+  }
+}]);
